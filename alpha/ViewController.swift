447fd9e2ff85da3add4be375e5a6f1230aef570f
autoconf <ceburakyilmaz@gmail.com>
2017-08-18 00:21:18 +0300

    first project

--- alpha/ViewController.swift
+++ alpha/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UILabel!
+    @IBAction func login(_ sender: Any) {
+        message.text = "Welcome"
+        message.isHidden = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
